1413f0a515851367cee379ccba9ca940b43fdc54
AnandaCampelo <anandag.c@hotmail.com>
2022-05-18 19:59:11 -0300

    001 minigame

--- Reaction_Time-Base.py
+++ Reaction_Time-Base.py
@@ -3,8 +3,12 @@ import random
 
 pygame.init()
 
-screen = pygame.display.set_mode((800,800)) #main display surface object
+screen = pygame.display.set_mode((800,400)) #main display surface object
 clock = pygame.time.Clock() #measure time
+
+font = pygame.font.SysFont(None, 30)
+img = font.render('precione qualquer tecla para começar.', True, (255,255,255))
+screen.blit(img, (20, 20))
 
 current_time = 0
 button_pressed_time = 0
@@ -13,6 +17,8 @@ reaction_time = 0
 
 k = True
 a = True
+b = False
+c = False
 
 #game loop
 while k:
@@ -24,28 +30,38 @@ while k:
             sys.exit()
         if event.type == pygame.KEYDOWN and a == True:
             button_pressed_time = pygame.time.get_ticks()
-            screen.fill((0,255,0))
+            screen.fill((0,255,100))
             a = False
-        elif event.type == pygame.KEYDOWN and a == False:
-                reaction_time = pygame.time.get_ticks()
-                print(f'reaction time: {reaction_time}')
+        elif event.type == pygame.KEYDOWN and a == False and b == True:
+            screen.fill((0,0,0))
+            reaction_time = pygame.time.get_ticks()
+            print('\n' + f'tempo de reação: {reaction_time}' + '\n')
+            font = pygame.font.SysFont(None, 30)
+            img = font.render(f'tempo de reação: {reaction_time}', True, (255,255,255))
+            screen.blit(img, (20, 20))
+            a = True
+            b = False
+            c = True
+        elif event.type == pygame.KEYDOWN and a == False and b == False:
+            screen.fill((0,0,0))
+            font = pygame.font.SysFont(None, 30)
+            img = font.render('Muito Cedo! Clique novamente para recomeçar.', True, (255,255,255))
+            screen.blit(img, (20, 20))
+            a = True
 
     lista_tempo = []
     for i in range(2000,10000):
         lista_tempo.append(i)
 
     random_time = random.choice(lista_tempo)
-    print(random_time)
 
     #gets what milliseconds it is at the moment
     current_time = pygame.time.get_ticks()  
 
-    if  current_time - button_pressed_time > random_time:
-        screen.fill((250,0,0))
-    
-    if reaction_time > 0:
-        k = False
-
+    if current_time - button_pressed_time > random_time and a == False:
+        screen.fill((250,0,50))
+        b = True
+        
     #draw the frame painted in the while loop:
     pygame.display.flip()
 
