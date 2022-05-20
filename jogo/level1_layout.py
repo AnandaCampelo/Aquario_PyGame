45856c01600f859154bb5f90abc6d12610c35de4
AnandaCampelo <anandag.c@hotmail.com>
2022-05-20 13:36:00 -0300

    001

---
+++ jogo/level1_layout.py
@@ -0,0 +1,15 @@
+level_01 = [
+'                                        ',
+'                                    XXXX',
+'                           X   X        ',
+'     XX        XXXX    XXXXXXXXX        ',
+'     X          XX                      ',
+'     X          XX        XX     XXX    ',
+'     XXXXX     XXXX           X         ',
+'                              X         ',
+'  XXXX   XX     XXXXXXXX   XXXXX     X  ',
+]
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_01) * tile_size
