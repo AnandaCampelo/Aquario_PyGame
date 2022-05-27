7215e9c808021a62a2e090c963f020b086f40b99
Milai <milaicpcs@al.insper.edu.br>
2022-05-27 13:17:29 -0300

    forca - 002

--- minigames/jogo da forca/jogo.py
+++ minigames/jogo da forca/jogo.py
@@ -12,15 +12,14 @@ dicas = []
 for dica in dic.keys():
     dicas.append(dica)
 dica_escolhida = random.choice(dicas)
+dica_final = 'Dica: ' + dica_escolhida
 for dica,lista in dic.items():
          if dica == dica_escolhida:
-            palavra = random.choice(lista)
+            p = random.choice(lista)
+            palavra = p.upper()
             break
 
 tentativas = []
-palavra_escolhida = []
-palavra_escondida = []
-end_game = True
 chances = 0
 letra = ''
 
@@ -44,6 +43,26 @@ def desenho(window, chances):
     if chances >= 6:
         pygame.draw.line(window, (0,0,0), (300,340), (375,415), 10)
 
+def esconde_palavra(palavra,tentativas):
+    palavra_escondida = palavra
+    for i in range(0,len(palavra_escondida)):
+        if not palavra_escondida[i] in tentativas:
+            palavra_escondida = palavra_escondida.replace(palavra_escondida[i], '-')
+    return palavra_escondida
+
+def tentar_letra(tentativas, palavra, letra, chances):
+    if not letra in tentativas:
+        tentativas.append(letra)
+        if not letra in palavra:
+            chances += 1
+    return tentativas, chances
+
+def jogo(window,palavra_escondida):
+    mostrar_dica = font.render(dica_final, True, (0,0,0))
+    window.blit(mostrar_dica, (500,50))
+    mostrar_palavra = font.render(palavra_escondida, True, (0,0,0))
+    window.blit(mostrar_palavra, (200,500))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,5 +72,9 @@ while True:
             letra = str(pygame.key.name(event.key)).upper()
 
     desenho(window,chances)
+    palavra_escondida = esconde_palavra(palavra,tentativas)
+    tentativas, chances = tentar_letra(tentativas, palavra, letra, chances)
+    jogo(window,palavra_escondida)
+    #desenho(window,chances)
 
     pygame.display.update()
