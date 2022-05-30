4ecb0fc227e359dd952512edf8f75dca042c96f7
Isabella Leão <isabellallima23@gmail.com>
2022-05-30 16:35:09 -0300

    cobra - 002

--- minigames/jogo da cobrinha/principal.py
+++ minigames/jogo da cobrinha/principal.py
@@ -20,3 +20,45 @@ class Apple:
     def move(self):
         self.x = random.randint(1,24)*SIZE
         self.y = random.randint(1,19)*SIZE
+
+
+class Snake:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("block-1.jpg").convert()
+        self.direction = 'down'
+
+        self.length = 1
+        self.x = [40]
+        self.y = [40]
+
+    def move_left(self):
+        self.direction = 'left'
+
+    def move_right(self):
+        self.direction = 'right'
+
+    def move_up(self):
+        self.direction = 'up'
+
+    def move_down(self):
+        self.direction = 'down'
+
+    def walk(self):
+        # update body
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
+        # update head
+        if self.direction == 'left':
+            self.x[0] -= SIZE
+        if self.direction == 'right':
+            self.x[0] += SIZE
+        if self.direction == 'up':
+            self.y[0] -= SIZE
+        if self.direction == 'down':
+            self.y[0] += SIZE
+
+        self.draw()
+        
