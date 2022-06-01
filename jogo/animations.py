3ea3c03c27fa4428337d83a0f06c4e6f7b54e13b
Milai <milaicpcs@al.insper.edu.br>
2022-06-01 14:03:38 -0300

    007.7

--- jogo/animations.py
+++ jogo/animations.py
@@ -4,13 +4,10 @@ import pygame
 def import_folder(path):
     surface_list = []
 
-    print("import folder",path)
     for _,__,image_files in walk(path):
-        print("walking")
         for image in image_files:
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
-            print("loaded", full_path)
 
     return surface_list
