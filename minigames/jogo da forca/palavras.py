957040a76e7921bdf726c9fa9bd12f637e6e2f6d
AnandaCampelo <anandag.c@hotmail.com>
2022-06-07 10:16:00 -0300

    ajustes

--- minigames/jogo da forca/palavras.py
+++ minigames/jogo da forca/palavras.py
@@ -1,3 +1,3 @@
 dic = {'animal': ['golfinho','rinoceronte', 'salamandra', 'javali', 'cutia', 'joaninha'],
-    'roupa': ['cropped','blazer', 'cachecol', 'colete'],
+    'roupa': ['cropped','blazer', 'cachecol', 'colete','jeans'],
     'comida': ['acaraje','pamonha', 'bacalhau', 'churrasco', 'nhoque']}
