03b8f8bf604cbff748d1a1eb993a1d787ebdd365
AnandaCampelo <anandag.c@hotmail.com>
2022-06-07 21:59:45 -0300

    10.1

--- jogo/level_layout.py
+++ jogo/level_layout.py
@@ -6,11 +6,18 @@ level_01 = [
 'XX              XX                           XX',
 'XXX             XX                      XXXXXXX',
 'XXXXXX          XXXX            XX             ',
-'            X  XX         XXX                  ',
-'P        XXXX  XX      X  XXX       XXXXXXXXXXX',
-'XXXXXXXXXXXXX  XXXXXXXXX  XXXXXXXXXXXXXXXXXXXXX',
+'            X   X         XXX                  ',
+'P        XXXX   X      X  XXX       XXXXXXXXXXX',
+'XXXXXXXXXXXXX   XXXXXXXX  XXXXXXXXXXXXXXXXXXXXX',
 ]
 
 tile_size = 64
 screen_width = 1200
 screen_height = len(level_01) * tile_size
+
+level_0 = {
+    'terreno' : './graficos/level0/level 0 layout_Camada de Tiles 1.csv',
+    'ouro' : './graficos/level0/level 0 layout_ouro.csv',
+    'placas' : './graficos/level0/level 0 layout_placas.csv',
+    'player' : './graficos/level0/level 0 layout_player.csv'
+}
